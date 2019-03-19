7818c0f8a2b734ddbdbdd71a2fef8650623b3ff1
Ramsha <Ramshaafzal122@gmail.com>
2019-03-19 14:45:06 +0500

    das

--- resources/views/sports/index.blade.php
+++ resources/views/sports/index.blade.php
@@ -5,19 +5,19 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Mukta:300,400,700"> 
-    <link rel="stylesheet" href="{{ asset('fonts/icomoon/style.css') }}">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Mukta:300,400,700"> 
+    <link rel="stylesheet" href="{{ asset('sports/fonts/icomoon/style.css') }}">
 
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/magnific-popup.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/jquery-ui.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/owl.theme.default.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('sports/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('sports/css/magnific-popup.css') }}">
+    <link rel="stylesheet" href="{{ asset('sports/css/jquery-ui.css') }}">
+    <link rel="stylesheet" href="{{ asset('sports/css/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('sports/css/owl.theme.default.min.css') }}">
 
 
-    <link rel="stylesheet" href="{{ asset('css/aos.css') }}">
+    <link rel="stylesheet" href="{{ asset('sports/css/aos.css') }}">
 
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('sports/css/style.css') }}">
     
   </head>
   <body>
--- resources/views/sports/score.blade.php
+++ resources/views/sports/score.blade.php
@@ -9,13 +9,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Mukta:300,400,700"> 
-    <link rel="stylesheet" href="{{ asset('fonts/icomoon/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('sports/fonts/icomoon/style.css') }}">
 
     <link rel="stylesheet" href="{{ asset('sports/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/magnific-popup.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/jquery-ui.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/owl.theme.default.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('sports/css/magnific-popup.css') }}">
+    <link rel="stylesheet" href="{{ asset('sports/css/jquery-ui.css') }}">
+    <link rel="stylesheet" href="{{ asset('sports/css/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('sports/css/owl.theme.default.min.css') }}">
 
 
     <link rel="stylesheet" href="{{ asset('sports/css/aos.css') }}">
