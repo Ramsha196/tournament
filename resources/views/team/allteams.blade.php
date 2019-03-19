0a5bbbb0d43fcf2b5b6a41e2f4a63f42a4085e69
Ramsha <Ramshaafzal122@gmail.com>
2019-03-19 14:48:35 +0500

    styling

--- resources/views/team/allteams.blade.php
+++ resources/views/team/allteams.blade.php
@@ -7,19 +7,18 @@
 
  
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Mukta:300,400,700"> 
-    <link rel="stylesheet" href="{{ asset('fonts/icomoon/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('sports/fonts/icomoon/style.css') }}">
 
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/magnific-popup.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/jquery-ui.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/owl.theme.default.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('sports/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('sports/css/magnific-popup.css') }}">
+    <link rel="stylesheet" href="{{ asset('sports/css/jquery-ui.css') }}">
+    <link rel="stylesheet" href="{{ asset('sports/css/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('sports/css/owl.theme.default.min.css') }}">
 
 
-    <link rel="stylesheet" href="{{ asset('css/aos.css') }}">
+    <link rel="stylesheet" href="{{ asset('sports/css/aos.css') }}">
 
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    
+    <link rel="stylesheet" href="{{ asset('sports/css/style.css') }}">
   </head>
   <body>
   
