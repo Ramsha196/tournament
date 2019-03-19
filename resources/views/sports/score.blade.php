e3cf894ce1f5fdf4ae5bf72d5bb00e6736864a33
Ramsha <Ramshaafzal122@gmail.com>
2019-03-19 14:43:33 +0500

    ds

--- resources/views/sports/score.blade.php
+++ resources/views/sports/score.blade.php
@@ -11,14 +11,14 @@
    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Mukta:300,400,700"> 
     <link rel="stylesheet" href="{{ asset('fonts/icomoon/style.css') }}">
 
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('sports/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/magnific-popup.css') }}">
     <link rel="stylesheet" href="{{ asset('css/jquery-ui.css') }}">
     <link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/owl.theme.default.min.css') }}">
 
 
-    <link rel="stylesheet" href="{{ asset('css/aos.css') }}">
+    <link rel="stylesheet" href="{{ asset('sports/css/aos.css') }}">
 
     <link rel="stylesheet" href="{{ asset('sports/css/style.css') }}">
     
